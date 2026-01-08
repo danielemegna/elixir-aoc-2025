d0b753a5111dca6430aa880c133134296f865246
Daniele Megna <megna.dany@gmail.com>
2026-01-08 09:03:20 +0100

    Run the solution with the file: solved !(?)
    .... no, something is wrong, 969 is lower !

--- test/day01/main_test.exs
+++ test/day01/main_test.exs
@@ -19,10 +19,21 @@ defmodule Day01.MainTest do
     assert actual == 3
   end
 
+  test "solve first part with file" do
+    actual = Day01.Main.find_password_for(read_file_stream())
+    assert actual == 969
+  end
+
   defp as_file_stream(file_content) do
     file_content
     |> String.trim()
     |> String.split("\n")
     |> Stream.map(&(&1))
   end
+
+  defp read_file_stream() do
+    File.stream!("./test/day01/input.txt")
+    |> Stream.map(&String.trim/1)
+  end
+
 end
