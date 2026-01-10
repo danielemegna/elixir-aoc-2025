9c8ca0edafff3ef8da106f4281f97ca6aba1f8bd
Daniele Megna <megna.dany@gmail.com>
2026-01-10 13:23:53 +0100

    Add 3-digits parsing test cases from real input

--- test/day01/rotation_test.exs
+++ test/day01/rotation_test.exs
@@ -7,11 +7,13 @@ defmodule Day01.RotationTest do
   test "parse a left rotation" do
     assert Rotation.parse("L13") == {:left, 13}
     assert Rotation.parse("L7") == {:left, 7}
+    assert Rotation.parse("L587") == {:left, 587}
   end
 
   test "parse a right rotation" do
     assert Rotation.parse("R26") == {:right, 26}
     assert Rotation.parse("R9") == {:right, 9}
+    assert Rotation.parse("R686") == {:right, 686}
   end
 
   test "raise error message on unexpected string" do
