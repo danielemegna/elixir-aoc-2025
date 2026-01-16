7b4c7f3842d3eec4c8ce296b1aeb63db5d63bbd9
Daniele Megna <megna.dany@gmail.com>
2026-01-16 12:57:56 +0100

    Integrate Bank module in main: first part solved!

--- lib/day03/main.ex
+++ lib/day03/main.ex
@@ -1,7 +1,18 @@
+alias Day03.Bank
+
 defmodule Day03.Main do
 
-  def sum_of_the_maximum_joltage_for_banks(_file_lines_stream) do
-    357
+  def sum_of_the_maximum_joltage_for_banks(file_lines_stream) do
+    file_lines_stream
+    |> Stream.map(fn line -> to_battery_bank_array(line) end)
+    |> Stream.map(fn batteries -> Bank.calculate_largest_joltage(batteries) end)
+    |> Enum.sum()
+  end
+
+  defp to_battery_bank_array(string) do
+    string
+    |> String.graphemes()
+    |> Enum.map(fn char -> String.to_integer(char) end)
   end
 
 end
