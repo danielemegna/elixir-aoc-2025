88b395165db2525633a8045c4bddff98a4d234e5
Daniele Megna <megna.dany@gmail.com>
2026-01-19 08:04:03 +0100

    Bad code to solve the goal

--- lib/day02/ids_range.ex
+++ lib/day02/ids_range.ex
@@ -2,18 +2,32 @@ defmodule Day02.IDsRange do
   def invalid_in({from, to}) do
     from_str = Integer.to_string(from)
     from_digits = String.length(from_str)
-
-    if(rem(from_digits, 2) == 0) do
-      {first_half, _} = from_str |> String.split_at(div(from_digits, 2))
-      candidate = String.to_integer(first_half <> first_half)
-
-      if candidate > from and candidate < to do
-        [candidate]
-      else
-        []
-      end
+    from_invalid_ids = if(rem(from_digits, 2) == 0) do
+      {half_str, _} = from_str |> String.split_at(div(from_digits, 2))
+      invalid_from(half_str, {from, to})
     else
       []
+    end
+
+    to_str = Integer.to_string(to)
+    to_digits = String.length(to_str)
+    to_invalid_ids = if(rem(to_digits, 2) == 0) do
+      {half_str, _} = to_str |> String.split_at(div(to_digits, 2))
+      invalid_from(half_str, {from, to})
+    else
+      []
+    end
+
+    Enum.concat(from_invalid_ids, to_invalid_ids) |> Enum.uniq()
+  end
+
+  defp invalid_from(half_str, {from, to}) do
+    candidate = String.to_integer(half_str <> half_str)
+
+    cond do
+      candidate < from -> []
+      candidate > to -> []
+      true -> [candidate]
     end
   end
 end
