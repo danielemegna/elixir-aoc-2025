0d31ba7d3f65a0313e311c678bf2d6c94027fc65
Daniele Megna <megna.dany@gmail.com>
2026-01-23 08:31:05 +0100

    Add list assertion helpers in test_helper file

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,17 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  use ExUnit.Case
+
+  def assert_list_contains(list, to_contain) do
+    missing = to_contain -- list
+
+    assert missing == [],
+           "Expected elements #{inspect(missing)} to be present in #{inspect(list)} collection."
+  end
+
+  def assert_lists_equal(first, second) do
+    assert_list_contains(first, second)
+    assert_list_contains(second, first)
+  end
+end
