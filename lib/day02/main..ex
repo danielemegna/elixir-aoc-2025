7377b6e6d21f8d2b2a27e5e5e92ee0fa94068bac
Daniele Megna <megna.dany@gmail.com>
2026-01-23 08:47:56 +0100

    Integrate IDsRange in main adding input parse: day02 first part solved! :tada:

--- lib/day02/main..ex
+++ lib/day02/main..ex
@@ -1,7 +1,21 @@
-defmodule Day02.Main do
+alias Day02.IDsRange
 
-  def invalid_ids_sum(_file_lines_stream) do
-    1227775554
+defmodule Day02.Main do
+  def invalid_ids_sum(file_lines_stream) do
+    line = file_lines_stream |> Enum.at(0)
+    ranges = parse_line_of_ranges(line)
+
+    ranges
+    |> Enum.flat_map(&IDsRange.invalid_in(&1))
+    |> Enum.sum()
   end
 
+  defp parse_line_of_ranges(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(fn raw_range ->
+      [from, to] = String.split(raw_range, "-")
+      {String.to_integer(from), String.to_integer(to)}
+    end)
+  end
 end
