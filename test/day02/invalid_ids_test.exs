4ea9d497db594441ff3055f0e1d0e73393ad6e45
Daniele Megna <megna.dany@gmail.com>
2026-02-05 08:56:04 +0100

    Add next failing tests for InvalidIDs

--- test/day02/invalid_ids_test.exs
+++ test/day02/invalid_ids_test.exs
@@ -12,4 +12,11 @@ defmodule Day02.InvalidIDsTest do
     assert_lists_equal(InvalidIDs.find_for({1234, 1550}), [1313, 1414, 1515])
   end
 
+  test "different number of digits in range" do
+    assert_lists_equal(InvalidIDs.find_for({90, 150}), [99, 111])
+    assert_lists_equal(InvalidIDs.find_for({880, 12000}), [
+      888, 999, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 11111
+    ])
+  end
+
 end
