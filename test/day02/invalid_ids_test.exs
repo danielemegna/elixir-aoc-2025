454b446f23e18436b4dc734be5a7d2de73aa65bc
Daniele Megna <megna.dany@gmail.com>
2026-02-09 07:49:35 +0100

    Fix InvalidIDs test: i was missing lot of invalid ids !

--- test/day02/invalid_ids_test.exs
+++ test/day02/invalid_ids_test.exs
@@ -14,9 +14,12 @@ defmodule Day02.InvalidIDsTest do
 
   test "different number of digits in range" do
     assert_lists_equal(InvalidIDs.find_for({90, 150}), [99, 111])
-    #assert_lists_equal(InvalidIDs.find_for({880, 12000}), [
-    #  888, 999, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 11111
-    #])
+    assert_list_contains(InvalidIDs.find_for({880, 12000}), [
+      888, 999, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 11111
+    ])
+    assert_list_contains(InvalidIDs.find_for({880, 12000}), [
+      1010, 1111, 1212, 1313, 1414, 1515, 1616, 2020, 2121
+    ])
   end
 
 end
