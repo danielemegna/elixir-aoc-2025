a30590e8415757e224755eab683f639af1c33e12
Daniele Megna <megna.dany@gmail.com>
2026-02-11 07:31:18 +0100

    Great: now IDsRange and InvalidIDs are the same with a `mode` param !

--- lib/day02/ids_range.ex
+++ lib/day02/ids_range.ex
@@ -1,35 +1,45 @@
 defmodule Day02.IDsRange do
 
-  def invalid_in({from, to}) do
+  def invalid_in({from, to}, mode \\ :single_repetition) do
     from_str = from |> Integer.to_string()
     to_str = to |> Integer.to_string()
     to_str_length = String.length(to_str)
 
-    half = ceil(to_str_length / 2)
+    half_length = ceil(to_str_length / 2)
+    max_repetitions = case mode do
+      :more_repetitions -> to_str_length
+      :single_repetition -> 2
+    end
 
     padded_from_str = from_str |> String.pad_leading(to_str_length, "0")
-    invalid_ids = Enum.reduce(1..half, [], fn chunk_size, invalid_ids ->
-      chunk = padded_from_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
-      limit = to_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
-      invalid_ids ++ find_with(chunk, 2, limit, {from, to})
+    invalid_ids = Enum.reduce(1..half_length, [], fn chunk_size, invalid_ids ->
+      number_to_repeat = padded_from_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
+      max_number = to_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
+      invalid_ids ++ find_with(number_to_repeat, max_number, 2, max_repetitions, {from, to})
     end)
 
     Enum.uniq(invalid_ids)
   end
 
-  defp find_with(chunk, repetition, limit, {from, to}) do
-    candidate = chunk
+  defp find_with(number_to_repeat, max_number, repetitions, max_repetitions, {from, to}) do
+    candidate = number_to_repeat
       |> Integer.to_string()
-      |> String.duplicate(repetition)
+      |> String.duplicate(repetitions)
       |> String.to_integer()
 
     cond do
       candidate == 0 ->
-        find_with(chunk+1, 2, limit, {from, to})
+        find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})
+      candidate < from and repetitions < max_repetitions ->
+        find_with(number_to_repeat, max_number, repetitions+1, max_repetitions, {from, to})
       in_range(candidate, {from, to}) ->
-        [candidate | find_with(chunk+1, 2, limit, {from, to})]
-      chunk < limit ->
-        find_with(chunk+1, 2, limit, {from, to})
+        if repetitions < max_repetitions do
+          [candidate | find_with(number_to_repeat, max_number, repetitions+1, max_repetitions, {from, to})]
+        else
+          [candidate | find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})]
+        end
+      number_to_repeat < max_number ->
+        find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})
       true ->
         []
     end
--- lib/day02/invalid_ids.ex
+++ lib/day02/invalid_ids.ex
@@ -1,23 +1,27 @@
 defmodule Day02.InvalidIDs do
 
-  def find_for({from, to}) do
+  def find_for({from, to}, mode \\ :more_repetitions) do
     from_str = from |> Integer.to_string()
     to_str = to |> Integer.to_string()
     to_str_length = String.length(to_str)
 
     half_length = ceil(to_str_length / 2)
+    max_repetitions = case mode do
+      :more_repetitions -> to_str_length
+      :single_repetition -> 2
+    end
 
     padded_from_str = from_str |> String.pad_leading(to_str_length, "0")
     invalid_ids = Enum.reduce(1..half_length, [], fn chunk_size, invalid_ids ->
       number_to_repeat = padded_from_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
       max_number = to_str |> String.split_at(chunk_size) |> elem(0) |> String.to_integer()
-      invalid_ids ++ find_with(number_to_repeat, max_number, 2, {from, to})
+      invalid_ids ++ find_with(number_to_repeat, max_number, 2, max_repetitions, {from, to})
     end)
 
     Enum.uniq(invalid_ids)
   end
 
-  defp find_with(number_to_repeat, max_number, repetitions, {from, to}) do
+  defp find_with(number_to_repeat, max_number, repetitions, max_repetitions, {from, to}) do
     candidate = number_to_repeat
       |> Integer.to_string()
       |> String.duplicate(repetitions)
@@ -25,13 +29,17 @@ defmodule Day02.InvalidIDs do
 
     cond do
       candidate == 0 ->
-        find_with(number_to_repeat+1, max_number, 2, {from, to})
-      candidate < from ->
-        find_with(number_to_repeat, max_number, repetitions+1, {from, to})
+        find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})
+      candidate < from and repetitions < max_repetitions ->
+        find_with(number_to_repeat, max_number, repetitions+1, max_repetitions, {from, to})
       in_range(candidate, {from, to}) ->
-        [candidate | find_with(number_to_repeat, max_number, repetitions+1, {from, to})]
+        if repetitions < max_repetitions do
+          [candidate | find_with(number_to_repeat, max_number, repetitions+1, max_repetitions, {from, to})]
+        else
+          [candidate | find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})]
+        end
       number_to_repeat < max_number ->
-        find_with(number_to_repeat+1, max_number, 2, {from, to})
+        find_with(number_to_repeat+1, max_number, 2, max_repetitions, {from, to})
       true ->
         []
     end
