f113176f99f47dfaa1f83d1735a268d97f24d597
Daniele Megna <megna.dany@gmail.com>
2026-02-18 07:51:04 +0100

    Improve readability of Bank.calculate_largest_joltage
    replace reduce operation with a recursive function
    and extract some middle-variables to improve readability

--- lib/day03/bank.ex
+++ lib/day03/bank.ex
@@ -1,25 +1,25 @@
 defmodule Day03.Bank do
 
   def calculate_largest_joltage(batteries, number_of_batteries_to_turn_on \\ 2) do
+    batteries_to_turn_on = batteries_to_turn_on(batteries, number_of_batteries_to_turn_on)
+    to_int(batteries_to_turn_on)
+  end
 
-    {batteries_to_turn_on, _} = Enum.reduce(
-      Range.new(number_of_batteries_to_turn_on-1, 0, -1),
-      {[], batteries},
-      fn(number_of_batteries_to_exclude, {batteries_to_turn_on, remaining_batteries}) ->
-        {battery_to_turn_on, battery_to_turn_on_index} = remaining_batteries
-        |> Enum.drop(-number_of_batteries_to_exclude)
-        |> max_with_index()
+  defp batteries_to_turn_on(remaining_batteries, remaining_batteries_to_turn_on, batteries_to_turn_on \\ []) do
+    number_of_batteries_to_exclude = remaining_batteries_to_turn_on - 1
+    candidates = remaining_batteries |> Enum.drop(-number_of_batteries_to_exclude)
+    {battery_to_turn_on, battery_to_turn_on_index} = max_with_index(candidates)
+    new_batteries_to_turn_on = [battery_to_turn_on | batteries_to_turn_on]
 
-        {
-          [battery_to_turn_on | batteries_to_turn_on],
-          Enum.drop(remaining_batteries, battery_to_turn_on_index + 1)
-        }
-      end
-    )
-
-    batteries_to_turn_on
-    |> Enum.reverse()
-    |> to_int()
+    if(remaining_batteries_to_turn_on == 1) do
+      new_batteries_to_turn_on |> Enum.reverse()
+    else
+      batteries_to_turn_on(
+        Enum.drop(remaining_batteries, battery_to_turn_on_index + 1),
+        remaining_batteries_to_turn_on - 1,
+        new_batteries_to_turn_on
+      )
+    end
   end
 
   defp to_int(digits) do
@@ -33,5 +33,4 @@ defmodule Day03.Bank do
     |> Enum.with_index()
     |> Enum.max_by(fn {value, _index} -> value end)
   end
-
 end
