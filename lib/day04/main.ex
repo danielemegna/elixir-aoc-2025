82ade80d862fda0c86723895e8a14d6858a70a2a
Daniele Megna <megna.dany@gmail.com>
2026-02-20 07:13:28 +0100

    Starting Day04 with acceptance test and fake implementation

---
+++ lib/day04/main.ex
@@ -0,0 +1,7 @@
+defmodule Day04.Main do
+
+  def accessible_rolls_of_paper_in(_map_lines_stream) do
+    13
+  end
+
+end
---
+++ test/day04/main_test.exs
@@ -0,0 +1,42 @@
+alias Day04.Main
+
+defmodule Day04.MainTest do
+  use ExUnit.Case
+
+  @provided_example_input """
+  ..@@.@@@@.
+  @@@.@.@.@@
+  @@@@@.@.@@
+  @.@@@@..@.
+  @@.@@@@.@@
+  .@@@@@@@.@
+  .@.@.@.@@@
+  @.@@@.@@@@
+  .@@@@@@@@.
+  @.@.@@@.@.
+  """
+
+  test "solve first part with provided example" do
+    actual = Main.accessible_rolls_of_paper_in(as_file_stream(@provided_example_input))
+    assert actual == 13
+  end
+
+  @tag :skip
+  test "solve first part with file" do
+    actual = Main.accessible_rolls_of_paper_in(read_file_stream())
+    assert actual == -1
+  end
+
+  defp as_file_stream(file_content) do
+    file_content
+    |> String.trim()
+    |> String.split("\n")
+    |> Stream.map(& &1)
+  end
+
+  defp read_file_stream() do
+    File.stream!("./test/day04/input.txt")
+    |> Stream.map(&String.trim/1)
+  end
+
+end
