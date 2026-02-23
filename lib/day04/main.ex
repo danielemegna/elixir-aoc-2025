54d5329a1082b4e61b4c5c3127b43b32876301ec
Daniele Megna <megna.dany@gmail.com>
2026-02-23 08:44:34 +0100

    Integrate RoomMap remove operations in main
    implement a recursive calc_total_removable_rolls_of_paper to
    repeatedly change the map removing removable paper rolls

--- lib/day04/main.ex
+++ lib/day04/main.ex
@@ -11,8 +11,21 @@ defmodule Day04.Main do
   end
 
   def total_removable_rolls_of_paper_in(map_lines_stream) do
-    _parsed_map = MapParser.parse(map_lines_stream)
-    43
+    parsed_map = MapParser.parse(map_lines_stream)
+    calc_total_removable_rolls_of_paper(parsed_map)
+  end
+
+  defp calc_total_removable_rolls_of_paper(room_map, counter \\ 0) do
+    removable_coordinates = RoomMap.get_removable(room_map)
+    removable_coordinates_count = Enum.count(removable_coordinates)
+    if removable_coordinates_count == 0 do
+      counter
+    else
+      new_map = Enum.reduce(removable_coordinates, room_map, fn coordinate, room_map ->
+        RoomMap.remove_paper_roll(room_map, coordinate)
+      end)
+      calc_total_removable_rolls_of_paper(new_map, counter + removable_coordinates_count)
+    end
   end
 
 end
