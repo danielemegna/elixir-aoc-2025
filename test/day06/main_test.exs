d57dba6e85db4e034e8da857a48a7d1afb13b6e9
Daniele Megna <megna.dany@gmail.com>
2026-03-11 08:02:03 +0100

    Enable second part acceptance tests: day06 solved! :tada:

--- test/day06/main_test.exs
+++ test/day06/main_test.exs
@@ -20,16 +20,14 @@ defmodule Day06.MainTest do
     assert actual == 3_968_933_219_902
   end
 
-  @tag :skip
   test "solve second part with provided example" do
     actual = Main.math_worksheet_answers_sum(as_file_stream(@provided_example_input), :right_to_left_columns)
     assert actual == (356 * 24 * 1) + (8 + 248 + 369) + 3_253_600 + (4 + 431 + 623)
   end
 
-  @tag :skip
   test "solve second part with file" do
     actual = Main.math_worksheet_answers_sum(read_file_stream(), :right_to_left_columns)
-    assert actual == -1
+    assert actual == 6019576291014
   end
 
   defp as_file_stream(file_content) do
