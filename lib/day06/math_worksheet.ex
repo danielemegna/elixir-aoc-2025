6e0e1dac723515d5ec5a89b7be7bee557f303556
Daniele Megna <megna.dany@gmail.com>
2026-03-11 08:07:26 +0100

    Some refactor in MathWorksheet parsing

--- lib/day06/math_worksheet.ex
+++ lib/day06/math_worksheet.ex
@@ -1,37 +1,9 @@
 defmodule Day06.MathWorksheet do
 
-  def parse_lines(lines, :common_vertical) do
-    {numbers_lines, [operators_line]} = lines |> Enum.split(-1)
-
-    operators = parse_operators_line(operators_line)
-
-    numbers = numbers_lines
-    |> Stream.map(fn line ->
-      Regex.scan(~r/\d+/, line)
-      |> List.flatten()
-      |> Enum.map(&String.to_integer/1)
-    end)
-    |> transpose()
-
-    Enum.zip(operators, numbers)
-  end
-
-  def parse_lines(lines, :right_to_left_columns) do
+  def parse_lines(lines, mode) do
     {numbers_lines, [operators_line]} = lines |> Enum.split(-1)
     operators = parse_operators_line(operators_line)
-
-    numbers = numbers_lines
-    |> Stream.map(&String.graphemes/1)
-    |> transpose()
-    |> Stream.map(fn line -> line |> Enum.join() |> String.trim() end)
-    |> Enum.reduce([[]], fn elem, [parsing | rest] = acc ->
-      case elem do
-        "" -> [[] | acc]
-        number -> [[String.to_integer(number) | parsing] | rest]
-      end
-    end)
-    |> Enum.reverse()
-
+    numbers = parse_numbers_lines(numbers_lines, mode)
     Enum.zip(operators, numbers)
   end
 
@@ -43,6 +15,28 @@ defmodule Day06.MathWorksheet do
 
   defp operator_atom("*"), do: :mult
   defp operator_atom("+"), do: :sum
+
+  defp parse_numbers_lines(lines, :common_vertical) do
+    lines
+    |> Stream.map(fn line ->
+      Regex.scan(~r/\d+/, line)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> transpose()
+  end
+
+  defp parse_numbers_lines(lines, :right_to_left_columns) do
+    lines
+    |> Stream.map(&String.graphemes/1)
+    |> transpose()
+    |> Stream.map(fn line -> line |> Enum.join() |> String.trim() end)
+    |> Enum.reduce([[]], fn
+      "", acc -> [[] | acc]
+      number, [completing | rest] -> [[String.to_integer(number) | completing] | rest]
+    end)
+    |> Enum.reverse()
+  end
 
   defp transpose(enum), do: Stream.zip_with(enum, &Function.identity/1)
 
