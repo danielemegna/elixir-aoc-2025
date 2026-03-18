357c9a18754e60b5d81b55e5a8203de90152b6d0
Daniele Megna <megna.dany@gmail.com>
2026-03-18 07:52:14 +0100

    Starting day07 with acceptance test and dummy implementation

---
+++ lib/day07/main.ex
@@ -0,0 +1,7 @@
+defmodule Day07.Main do
+
+  def beam_split_count(_file_lines_stream) do
+    21
+  end
+
+end
---
+++ test/day07/main_test.exs
@@ -0,0 +1,48 @@
+alias Day07.Main
+
+defmodule Day07.MainTest do
+  use ExUnit.Case
+
+  @provided_example_input """
+  .......S.......
+  ...............
+  .......^.......
+  ...............
+  ......^.^......
+  ...............
+  .....^.^.^.....
+  ...............
+  ....^.^...^....
+  ...............
+  ...^.^...^.^...
+  ...............
+  ..^...^.....^..
+  ...............
+  .^.^.^.^.^...^.
+  ...............
+  """
+
+  test "solve first part with provided example" do
+    actual = Main.beam_split_count(as_file_stream(@provided_example_input))
+    assert actual == 21
+  end
+
+  @tag :skip
+  test "solve first part with file" do
+    actual = Main.beam_split_count(read_file_stream())
+    assert actual == -1
+  end
+
+  defp as_file_stream(file_content) do
+    file_content
+    |> String.trim()
+    |> String.split("\n")
+    |> Stream.map(& &1)
+  end
+
+  defp read_file_stream() do
+    File.stream!("./test/day07/input.txt")
+    |> Stream.map(&String.trim/1)
+  end
+
+end
