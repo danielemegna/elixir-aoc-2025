97175aa0946947d75e8b0b3f5b04c6001229882c
Daniele Megna <megna.dany@gmail.com>
2026-03-24 07:37:04 +0100

    Nice recursive solution... but it do not solve with file due performance :(

--- lib/day07/main.ex
+++ lib/day07/main.ex
@@ -13,8 +13,25 @@ defmodule Day07.Main do
     final_state.splits
   end
 
-  def different_timelines_count(_file_lines_stream) do
-    40
+  def different_timelines_count(file_lines_stream) do
+    [first_line | rest] = Enum.take_every(file_lines_stream, 2)
+    [_, {starting_x, _}] = Regex.run(~r/\.+(S)\.+/, first_line, return: :index)
+    different_timelines_with(starting_x, rest)
   end
+
+  defp different_timelines_with(beam_position, [line | rest]) do
+    splitters =
+      Regex.scan(~r/\^/, line, return: :index)
+      |> Enum.map(fn [{splitter_x, _}] -> splitter_x end)
+
+    if Enum.member?(splitters, beam_position) do
+      different_timelines_with(beam_position + 1, rest) +
+        different_timelines_with(beam_position - 1, rest)
+    else
+      different_timelines_with(beam_position, rest)
+    end
+  end
+
+  defp different_timelines_with(_beam_position, []), do: 1
 
 end
