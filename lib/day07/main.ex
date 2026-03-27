4c27b8a2aa46c0331465cfce5c9328ab1dd27b5d
Daniele Megna <megna.dany@gmail.com>
2026-03-27 08:23:30 +0100

    Use new main functions and BeamStateV2 also for beam_split_count
    BeamStateV2 solution properly solve also first part :nail_care:

--- lib/day07/main.ex
+++ lib/day07/main.ex
@@ -1,14 +1,12 @@
-alias Day07.BeamState
 alias Day07.TachyonDiagram
 
 defmodule Day07.Main do
 
   def beam_split_count(file_lines_stream) do
-    final_state = file_lines_stream
-    |> Enum.take_every(2)
-    |> Enum.reduce(%BeamState{}, fn line, beam_state ->
-      TachyonDiagram.consume(line, beam_state)
-    end)
+    {first_line, splitters_lines} = split_input_file_lines(file_lines_stream)
+    final_state = first_line
+    |> TachyonDiagram.init_beam_state_from()
+    |> TachyonDiagram.consume_stream(splitters_lines)
 
     final_state.splits
   end
