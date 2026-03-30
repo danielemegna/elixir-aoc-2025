f6fe5d24e9d145b28aeb074b44eb7e0d6145bbf1
Daniele Megna <megna.dany@gmail.com>
2026-03-30 07:31:49 +0200

    Improve day07 main reducing duplication

--- lib/day07/main.ex
+++ lib/day07/main.ex
@@ -3,21 +3,19 @@ alias Day07.TachyonDiagram
 defmodule Day07.Main do
 
   def beam_split_count(file_lines_stream) do
-    {first_line, splitters_lines} = split_input_file_lines(file_lines_stream)
-    final_state = first_line
-    |> TachyonDiagram.init_beam_state_from()
-    |> TachyonDiagram.consume_stream(splitters_lines)
-
+    final_state = consume(file_lines_stream)
     final_state.splits
   end
 
   def different_timelines_count(file_lines_stream) do
-    {first_line, splitters_lines} = split_input_file_lines(file_lines_stream)
-    final_state = first_line
-    |> TachyonDiagram.init_beam_state_from()
-    |> TachyonDiagram.consume_stream(splitters_lines)
-
+    final_state = consume(file_lines_stream)
     final_state.beams |> Map.values() |> Enum.sum()
+  end
+
+  defp consume(file_lines_stream) do
+    {first_line, splitters_lines} = split_input_file_lines(file_lines_stream)
+    initial_state = TachyonDiagram.init_beam_state_from(first_line)
+    TachyonDiagram.consume_stream(initial_state, splitters_lines)
   end
 
   defp split_input_file_lines(file_lines_stream) do
