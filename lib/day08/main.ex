ac46015f668b21855d44c988f88de7ae55a204ee
Daniele Megna <megna.dany@gmail.com>
2026-04-09 08:11:53 +0200

    Extract junction_boxes_distances_from(lines) to be reused

--- lib/day08/main.ex
+++ lib/day08/main.ex
@@ -4,17 +4,7 @@ alias Day08.Circuits
 defmodule Day08.Main do
 
   def three_largest_circuits_product(file_lines_stream, junction_boxes_to_connect) do
-    junction_boxes_distances = file_lines_stream
-    |> Enum.map(&(parse_line(&1)))
-    |> Enum.reduce({[], []}, fn junction_box, {distances, computed} ->
-      new_distances = computed
-      |> Enum.map(fn other ->
-        distance = JunctionBoxes.euclidean_distance(junction_box, other)
-        {distance, [junction_box, other]}
-      end)
-      {new_distances ++ distances, [junction_box | computed]}
-    end)
-    |> elem(0)
+    junction_boxes_distances = junction_boxes_distances_from(file_lines_stream)
 
     shortest_junction_boxes_distances = junction_boxes_distances
     |> Enum.sort()
@@ -31,6 +21,22 @@ defmodule Day08.Main do
 
   def last_connected_junction_boxes_x_product(_file_lines_stream) do
     25272
+  end
+
+  defp junction_boxes_distances_from(file_lines_stream) do
+    file_lines_stream
+    |> Enum.map(&parse_line(&1))
+    |> Enum.reduce({[], []}, fn junction_box, {distances, computed} ->
+      new_distances =
+        computed
+        |> Enum.map(fn other ->
+          distance = JunctionBoxes.euclidean_distance(junction_box, other)
+          {distance, [junction_box, other]}
+        end)
+
+      {new_distances ++ distances, [junction_box | computed]}
+    end)
+    |> elem(0)
   end
 
   defp create_circuits([], circuits), do: circuits
@@ -58,7 +64,7 @@ defmodule Day08.Main do
   defp parse_line(file_line) do
     file_line
     |> String.split(",")
-    |> Enum.map(&(String.to_integer(&1)))
+    |> Enum.map(&String.to_integer/1)
     |> List.to_tuple()
   end
 
