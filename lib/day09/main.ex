7c199a9e335b1841b9392c9f533751cd75ec17d4
Daniele Megna <megna.dany@gmail.com>
2026-04-27 08:20:01 +0200

    Refactor day 9 solution of first part

--- lib/day09/main.ex
+++ lib/day09/main.ex
@@ -1,36 +1,34 @@
 defmodule Day09.Main do
+
   def largest_red_tiles_rectangle_with(file_lines_stream) do
-    parsed =
-      file_lines_stream
-      |> Enum.map(fn str ->
-        str
-        |> String.split(",")
-        |> Enum.map(&String.to_integer/1)
-        |> List.to_tuple()
-      end)
+    file_lines_stream
+    |> parse_red_tiles_locations()
+    |> largest_rectangle_area_for()
+  end
 
-    {largest_rectagle, _} =
-      Enum.reduce(parsed, {0, Enum.drop(parsed, 1)}, fn
-        _, {max, []} ->
-          {max, []}
+  defp largest_rectangle_area_for(locations, current_max \\ 0)
+  defp largest_rectangle_area_for([_ | []], current_max), do: current_max
 
-        point, {max, rest} ->
-          current_max =
-            rest
-            |> Enum.map(fn other -> rectangle_area_with(point, other) end)
-            |> Enum.max()
+  defp largest_rectangle_area_for([first_location | other_locations], current_max) do
+    this_max = other_locations
+      |> Enum.map(fn other -> rectangle_area_with(first_location, other) end)
+      |> Enum.max()
 
-          if(current_max > max) do
-            {current_max, Enum.drop(rest, 1)}
-          else
-            {max, Enum.drop(rest, 1)}
-          end
-      end)
-
-    largest_rectagle
+    largest_rectangle_area_for(other_locations, max(this_max, current_max))
   end
 
   defp rectangle_area_with({x1, y1}, {x2, y2}) do
-    abs(x1 - x2 + 1) * abs(y1 - y2 + 1)
+    (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
   end
+
+  defp parse_red_tiles_locations(file_lines_stream) do
+    file_lines_stream
+    |> Enum.map(fn str ->
+      str
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
 end
