f70c0b254d36db6aca35195692622b7a7ed1e1d3
Daniele Megna <megna.dany@gmail.com>
2026-09-11 08:55:40 +0200

    Calculate green_tiles building the TileFloor to complete is_colorful
    recognize green borders as colorful

--- test/day09/tile_floor_test.exs
+++ test/day09/tile_floor_test.exs
@@ -22,6 +22,14 @@ defmodule Day09.TileFloorTest do
       assert TileFloor.is_colorful(@tile_floor, {7, 3}) == true
     end
 
+    test "green tile" do
+      assert TileFloor.is_colorful(@tile_floor, {9, 1}) == true
+      assert TileFloor.is_colorful(@tile_floor, {11, 2}) == true
+      assert TileFloor.is_colorful(@tile_floor, {10, 7}) == true
+      assert TileFloor.is_colorful(@tile_floor, {4, 5}) == true
+      assert TileFloor.is_colorful(@tile_floor, {2, 4}) == true
+    end
+
   end
 
 end
