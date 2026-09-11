a57ad4e55a74af9a319f5d8b4118b77b28fdedcc
Daniele Megna <megna.dany@gmail.com>
2026-09-11 08:30:50 +0200

    Recognize red tiles as colorful in TileFloor

--- test/day09/tile_floor_test.exs
+++ test/day09/tile_floor_test.exs
@@ -3,14 +3,23 @@ alias Day09.TileFloor
 defmodule Day09.TileFloorTest do
   use ExUnit.Case
 
-  describe "build and check color" do
+  describe "build and check colorful tiles" do
+    @tile_floor TileFloor.new([
+      {7, 1}, {11, 1}, {11, 7}, {9, 7},
+      {9, 5}, {2, 5}, {2, 3}, {7, 3}
+    ])
 
-    test "non-green tile" do
-      tile_floor = TileFloor.new([
-        {7, 1}, {11, 1}, {11, 7}, {9, 7},
-        {9, 5}, {2, 5}, {2, 3}, {7, 3}
-      ])
-      assert TileFloor.is_green(tile_floor, {0,0}) == false
+    test "non-colorful tiles" do
+      assert TileFloor.is_colorful(@tile_floor, {0, 0}) == false
+      assert TileFloor.is_colorful(@tile_floor, {1, 2}) == false
+      assert TileFloor.is_colorful(@tile_floor, {12, 9}) == false
+    end
+
+    test "red tiles on border" do
+      assert TileFloor.is_colorful(@tile_floor, {7, 1}) == true
+      assert TileFloor.is_colorful(@tile_floor, {11, 1}) == true
+      assert TileFloor.is_colorful(@tile_floor, {2, 3}) == true
+      assert TileFloor.is_colorful(@tile_floor, {7, 3}) == true
     end
 
   end
