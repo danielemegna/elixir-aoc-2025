af1f3e00634dd3dd3fa8802b91124183b6754d12
Daniele Megna <megna.dany@gmail.com>
2026-09-15 07:18:19 +0200

    Recognize green tiles inside the borders in TileFloor

--- lib/day09/tile_floor.ex
+++ lib/day09/tile_floor.ex
@@ -16,8 +16,23 @@ defmodule Day09.TileFloor do
   end
 
   def is_colorful(%TileFloor{} = floor, subject) do
+    is_on_borders(floor, subject) ||
+        is_inside_borders(floor, subject)
+  end
+
+  defp is_on_borders(%TileFloor{} = floor, subject) do
     Enum.member?(floor.red_tiles, subject) ||
       Enum.member?(floor.green_tiles, subject)
+  end
+
+  defp is_inside_borders(%TileFloor{} = floor, {x, y}) do
+    Enum.reduce(0..y, false, fn current_y, is_inside ->
+      if(is_on_borders(floor, {x, current_y})) do
+        !is_inside
+      else
+        is_inside
+      end
+    end)
   end
 
   defp tiles_between({x, y1}, {x, y2}) do
--- test/day09/tile_floor_test.exs
+++ test/day09/tile_floor_test.exs
@@ -12,22 +12,33 @@ defmodule Day09.TileFloorTest do
     test "non-colorful tiles" do
       assert TileFloor.is_colorful(@tile_floor, {0, 0}) == false
       assert TileFloor.is_colorful(@tile_floor, {1, 2}) == false
+      assert TileFloor.is_colorful(@tile_floor, {5, 6}) == false
+      assert TileFloor.is_colorful(@tile_floor, {5, 7}) == false
       assert TileFloor.is_colorful(@tile_floor, {12, 9}) == false
     end
 
-    test "red tiles on border" do
+    test "red tiles are colorful" do
       assert TileFloor.is_colorful(@tile_floor, {7, 1}) == true
       assert TileFloor.is_colorful(@tile_floor, {11, 1}) == true
       assert TileFloor.is_colorful(@tile_floor, {2, 3}) == true
       assert TileFloor.is_colorful(@tile_floor, {7, 3}) == true
     end
 
-    test "green tile" do
+    test "green tile on borders are colorful" do
       assert TileFloor.is_colorful(@tile_floor, {9, 1}) == true
       assert TileFloor.is_colorful(@tile_floor, {11, 2}) == true
+      assert TileFloor.is_colorful(@tile_floor, {11, 3}) == true
+      assert TileFloor.is_colorful(@tile_floor, {11, 4}) == true
       assert TileFloor.is_colorful(@tile_floor, {10, 7}) == true
+      assert TileFloor.is_colorful(@tile_floor, {3, 5}) == true
       assert TileFloor.is_colorful(@tile_floor, {4, 5}) == true
+      assert TileFloor.is_colorful(@tile_floor, {5, 5}) == true
+      assert TileFloor.is_colorful(@tile_floor, {6, 5}) == true
       assert TileFloor.is_colorful(@tile_floor, {2, 4}) == true
+    end
+
+    test "green tile inside the borders are colorful" do
+      assert TileFloor.is_colorful(@tile_floor, {9, 3}) == true
     end
 
   end
